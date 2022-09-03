9f16c7dea88151f5966af6b82b6038d75c924a06
AchaCans <112678141+AchaCans@users.noreply.github.com>
2022-09-03 15:42:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['16199961931', 'Ziv San', true], 
-['6282179137771', 'HAORI IQ-MD', true] 
+['6289506219231', 'Jacky', true], 
+['6283841800798', 'Omen', true] 
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
